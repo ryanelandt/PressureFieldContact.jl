012251c56bd358748d01615b424e5eda4961c6ad
Ryan Elandt <ryan.elandt@gmail.com>
2018-09-25 22:19:20 -0400

    added MatrixTransform and utility funtios

--- src/SoftContact.jl
+++ src/SoftContact.jl
@@ -8,11 +8,30 @@ using LinearAlgebra
 using GeometryTypes: HomogenousMesh, Face, Point
 
 
+const frame_tet_cs = CartesianFrame3D("tet_cs")
+const frame_tri_cs = CartesianFrame3D("tri_cs")
+
+include("utility.jl")
+include("matrix_transform.jl")
 include("material.jl")
 include("mesh_cache.jl")
 include("mesh_inertia.jl")
 
 export
+    frame_tet_cs,
+    frame_tri_cs,
+
+    # utility.jl
+    unPad,
+    onePad,
+    zeroPad,
+    fill_with_nothing!,
+
+    # matrix_transform.jl
+    Point4D,
+    MatrixTransform,
+    getPoint,
+
     # material.jl
     ContactMaterialProperties,
     InertiaMaterialProperties,
@@ -32,4 +51,5 @@ export
     makeInertiaTensor,
     centroidVolumeCombo,
     equiv_volume
+
 end
