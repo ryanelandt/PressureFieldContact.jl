435a28a79c0d97249820ae6d7f02f3454c7c03d3
Ryan Elandt <ryan.elandt@gmail.com>
2018-10-07 11:01:03 -0400

    added more methods for asHomogenousMesh

--- src/mesh_cache.jl
+++ src/mesh_cache.jl
@@ -44,45 +44,17 @@ struct MeshCache
     end
 end
 
-# function addBodyMeshCache(point::Vector{SVector{3,Float64}}, name::String, tri_ind::Vector{SVector{3,Int64}}, tet_ind::Vector{SVector{4,Int64}},
-#     strain::Vector{Float64}, contact_prop::ContactProperties, inertia_prop::InertiaProperties, mechanism::Mechanism)
-#
-#     body = addBodyVolumeMesh!(mechanism, name, point, tet_ind, inertia_prop)
-#     return MeshCache(point, name, tri_ind, tet_ind, strain, contact_prop, body, inertia_prop)
-# end
-#
-# function addBodyMeshCache(point::Vector{SVector{3,Float64}}, name::String, tri_ind::Vector{SVector{3,Int64}}, inertia_prop::InertiaProperties, mechanism::Mechanism)
-#     body = addBodySurfaceMesh!(mechanism, name, point, tri_ind, inertia_prop)
-#     return MeshCache(point, name, tri_ind, body, inertia_prop)
-# end
-#
-# function addBodyVolumeMesh!(mechanism::Mechanism, name::String, point::Vector{SVector{3,Float64}}, tet_ind::Vector{SVector{4,Int64}}, inertia_prop::InertiaProperties)
-#     rho = inertia_prop.rho
-#     (inertia_prop.thickness == nothing) || error("assumed thickness is something but should be nothing")
-#     I3, com, mass, mesh_vol = makeInertiaTensor(point, tet_ind, rho)
-#     return addBodyMesh!(mechanism, name, I3, com, mass)
-# end
-#
-# function addBodySurfaceMesh!(mechanism::Mechanism, name::String, point::Vector{SVector{3,Float64}}, tri_ind::Vector{SVector{3,Int64}}, inertia_prop::InertiaProperties)
-#     rho = inertia_prop.rho
-#     thickness = inertia_prop.thickness
-#     (thickness == nothing) && error("assumed thickness is nothing")
-#     I3, com, mass, mesh_vol = makeInertiaTensor(point, tri_ind, rho, thickness)
-#     return addBodyMesh!(mechanism, name, I3, com, mass)
-# end
-#
-# function addBodyMesh!(mechanism::Mechanism, name::String, I3, com, mass)
-#     body_child = newBodyFromInertia(name, I3, com, mass)
-#     body_parent = root_body(mechanism)
-#     j_parent_child, x_parent_child = outputJointTransform_ParentChild(body_parent, body_child, SPQuatFloating{Float64}(), SVector{3,Float64}(0,0,0))
-#     attach!(mechanism, body_parent, body_child, j_parent_child, joint_pose=x_parent_child)
-#     return body_child
-# end
-
 function asHomogenousMesh(meshCache::MeshCache)
     vec_Face = Face{3, Int32}.(meshCache.tri.ind)
     vec_Point = Point{3, Float32}.(meshCache.point)
     return HomogenousMesh(vec_Point, vec_Face)
+end
+
+asHomogenousMesh(meshCache::MeshCache, color) = asHomogenousMesh(meshCache, RGBA{Float32}(color...))
+function asHomogenousMesh(meshCache::MeshCache, color::RGBA{Float32})
+    vec_Face = Face{3, Int32}.(meshCache.tri.ind)
+    vec_Point = Point{3, Float32}.(meshCache.point)
+    return HomogenousMesh(vec_Point, vec_Face, color)
 end
 
 @RigidBodyDynamics.indextype MeshID
