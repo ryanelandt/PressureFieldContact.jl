4b55b3180dcf236fca431632f06f8bfd153c6f5a
Ryan Elandt <ryan.elandt@gmail.com>
2018-10-12 18:48:22 -0400

    updates

--- test/spoon.jl
+++ test/spoon.jl
@@ -9,7 +9,7 @@ using ColorTypes: RGBA, RGB
 using MeshCatMechanisms
 using SoftContact
 using FileIO
-using NumericalTricks
+# using NumericalTricks
 using GeometryTypes: HomogenousMesh
 
 
@@ -31,14 +31,14 @@ add_volume_mesh!(ts, root_body(my_mechanism), lo_name, lo_h_mesh, lo_tet_mesh)
 up_name = "box_up"
 up_joint = Prismatic(SVector{3,Float64}(0.0, 0.0, 1.0))
 up_h_mesh, up_tet_mesh = create_volume_box(rad_box)
-add_body_volume_mesh!(ts, up_name, up_h_mesh, up_tet_mesh, InertiaProperties(rho=500.0), joint=up_joint)
+up_joint = add_body_volume_mesh!(ts, up_name, up_h_mesh, up_tet_mesh, InertiaProperties(rho=500.0), joint=up_joint)
 
 ### Spoon
 mesh_spoon = load(spoon_path)
 scale_HomogenousMesh!(mesh_spoon, 0.01)  # make 0.01 of origional size or spoon will be HUGE
 # assume that the spoon in a shell of this density and thickness (d) for the purposes of inertia tensor calculation
 spoon_i_prop = InertiaProperties(rho=900.0, d=0.0025)
-add_body_surface_mesh!(ts, "spoon", mesh_spoon, spoon_i_prop)  # adds body and calculates inertia from the surface mesh
+spoon_joint = add_body_surface_mesh!(ts, "spoon", mesh_spoon, spoon_i_prop)  # adds body and calculates inertia from the surface mesh
 
 ### Friction pairs
 add_pair_rigid_compliant_bristle_tune_tri!(ts, "spoon", up_name)
@@ -47,10 +47,8 @@ mech_scen = MechanismScenario(ts, calcXd!, n_quad_rule=1)  # there will be many 
 
 ### Set initial condition
 # NOTE: All states are zero initialized including deformation state variables
-spoon_rot = components(SPQuat(RotX(pi/2)))
-spoon_trans = SVector{3,Float64}(0.0, 0.0, 0.000001)
-set_configuration!(mech_scen.float.state, findjoint(my_mechanism, "world_spoon"), vcat(spoon_rot, spoon_trans))  # spoon just off ground
-set_configuration!(mech_scen.float.state, findjoint(my_mechanism, "world_" * up_name), [0.10])  # block to 10cm
+set_state_spq!(mech_scen, spoon_joint, rot=RotX(pi/2), trans=SVector(0.0, 0.0, 0.000001))
+set_configuration!(mech_scen.float.state, up_joint, [0.10])
 x = get_state(mech_scen)
 
 ### Run forward dynamics
@@ -68,7 +66,9 @@ end
 ### Add meshes to visualizer
 mvis = MechanismVisualizer(my_mechanism, vis)
 color_pad = RGBA{Float32}(1.0, 0.792, 0.588, 1.0)
-lo_h_mesh_32 = HomogenousMesh(vertices=get_h_mesh_vertices_32(lo_h_mesh), faces=get_h_mesh_faces_32(lo_h_mesh), color=color_pad)
+lo_h_mesh_32 = HomogenousMesh(vertices=get_h_mesh_vertices_32(lo_h_mesh),
+                              faces=get_h_mesh_faces_32(lo_h_mesh),
+                              color=color_pad)
 setobject!(vis, lo_h_mesh_32)
 set_body_mesh_visual!(mvis, mech_scen, "spoon", [0.5, 0.5, 0.5, 1.0])
 set_body_mesh_visual!(mvis, mech_scen, up_name, color_pad)
