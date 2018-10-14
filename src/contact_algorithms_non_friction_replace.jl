2860a09d295e52c13a387600e8be4adcaca10a43
Ryan Elandt <ryan.elandt@gmail.com>
2018-10-14 01:19:40 -0400

    updateds

--- src/contact_algorithms_non_friction_replace.jl
+++ src/contact_algorithms_non_friction_replace.jl
@@ -14,9 +14,9 @@ end
 
 function calc_ζ_transforms(frame_ζ::CartesianFrame3D, frame_r ::CartesianFrame3D, p_tet, x_r_w, x_w_r)
     x_r_ζ = MatrixTransform(frame_ζ, frame_r, asMatOnePad(p_tet))
-    x_w_ζ_top = getTop(x_w_r * x_r_ζ)
+    x_w_ζ = x_w_r * x_r_ζ
     x_ζ_w = inv(x_r_ζ) * x_r_w  # NOTE: inv(A_r¹_ζ) is **always** Float64
-    return x_w_ζ_top, x_ζ_w
+    return x_w_ζ, x_ζ_w
 end
 
 function find_plane_tet(E::Float64, ϵ::SVector{4,Float64}, X_r_w)
