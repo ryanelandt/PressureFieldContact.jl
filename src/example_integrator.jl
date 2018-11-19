769a81d6cbb479ec3ec1f523eb83f8c4811d0360
Ryan <ryan.elandt@gmail.com>
2018-11-18 21:50:47 -0500

    updated to handle varying time

--- src/example_integrator.jl
+++ src/example_integrator.jl
@@ -1,6 +1,6 @@
 
 function integrate_scenario_radau(rr::RadauIntegrator{MechanismScenario}, mech_scen::MechanismScenario,
-        x::Vector{Float64}; t_final::Float64=1.0, max_steps::Int64=1000)
+        x::Vector{Float64}; t_final::Float64=1.0, max_steps::Int64=1000, is_disp_count::Bool=false)
 
     n_dof = num_x(mech_scen)
     (length(x) == n_dof) || error("The length of x ($(length(x))) is different from the number of variables in the scenario ($n_dof). Did you forget the state variables for bristle friction?")
@@ -11,7 +11,8 @@ function integrate_scenario_radau(rr::RadauIntegrator{MechanismScenario}, mech_s
     data_state = zeros(Float64, max_steps, n_dof)
     verify_bristle_ids!(mech_scen, x)  # make sure than any bristle friction variables are used exactly once
     for k = 1:max_steps
-        h, x = solveRadau(rr, x)
+        is_disp_count && println(k)
+        h, x = solveRadau(rr, x, t_cumulative)
         principal_value!(mech_scen, x)
         t_cumulative += h
         data_time[k] = t_cumulative
