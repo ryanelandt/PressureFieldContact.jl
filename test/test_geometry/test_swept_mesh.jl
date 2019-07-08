1027b14168a4dc634fb4fcb3c7eb81228efe1ee5
“Ryan <ryan.elandt@gmail.com>
2019-07-08 19:14:35 -0400

    fixed bug and added test for variable radius swept mesh

--- test/test_geometry/test_swept_mesh.jl
+++ test/test_geometry/test_swept_mesh.jl
@@ -10,10 +10,21 @@ function is_convex_mesh_oriented_outward(eM::eMesh{Tri,T2}) where {T2}
 end
 
 @testset "swept_mesh" begin
+	# constant radius
 	eM_box = create_swept_mesh(f_swept_triv, [-1.0, 1.0], 1.0, 4, true)
 	@test is_convex_mesh_oriented_outward(eM_box)
 	@test 8.0 ≈ volume(eM_box)
 	@test verify_mesh(eM_box) == nothing
+
+	# variable radius
+	eM_box_2 = create_swept_mesh(f_swept_triv, [-1.0, 1.0], [1.0, 1.0], 4, true)
+	@test is_convex_mesh_oriented_outward(eM_box_2)
+	@test 8.0 ≈ volume(eM_box_2)
+	@test verify_mesh(eM_box_2) == nothing
+
+	for field_k = fieldnames(typeof(eM_box))
+		@test getfield(eM_box, field_k) == getfield(eM_box_2, field_k)
+	end
 
 	eM_box_check = eMesh_box()
 	@test is_convex_mesh_oriented_outward(eM_box_check)
